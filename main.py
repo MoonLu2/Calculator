57e43d6eade22dd633bf904fe55359a46f8c45ae
MoonyLu2 <153979873+MoonLu2@users.noreply.github.com>
2025-01-23 15:10:01 +0200

    Create main.py
    Calculator is now on github!

---
+++ main.py
@@ -0,0 +1,47 @@
+#Ввод операций и его проверка на правильность а иначе, ввод второго числа
+print('Операции: +, -, *, /')
+inp = input('>')
+if inp != '+' and inp != '-' and inp != '*' and inp != '/':
+    print('Ошибка: Ввод не содержит символов показанных выше. ПЖ повторите попытку :(')
+else:
+    print(inp2, inp)
+    print('Введите число.')
+    inp3 = float(input('>'))
+    print(inp2, inp, inp3)
+#--------------------
+
+#Функции операций
+def plus(inp2, inp3):
+    sum_ = inp2 + inp3
+    print('=', sum_)
+    return sum_
+
+def minus(inp2, inp3):
+    sum_ = inp2 - inp3
+    print('=', sum_)
+    return sum_
+
+def multiply(inp2, inp3):
+    sum_ = inp2 * inp3
+    print('=', sum_)
+    return sum_
+
+def devide(inp2, inp3):
+    sum_ = inp2 / inp3
+    print('=', sum_)
+    return sum_
+#--------------------
+
+#Условия операций
+if inp == '+':
+    plus(inp2, inp3)
+
+if inp == '-':
+    minus(inp2, inp3)
+
+if inp == '*':
+    multiply(inp2, inp3)
+
+if inp == '/':
+    devide(inp2, inp3)
+#--------------------
